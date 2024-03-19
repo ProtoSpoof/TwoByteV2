e0e105a45b111644cb5b07af08ba02f46540eb6d
Quinlan Kildare <46752388+ProtoSpoof@users.noreply.github.com>
2024-03-19 16:18:24 -0400

    prettify

--- packages/minecraft/index.js
+++ packages/minecraft/index.js
@@ -1,23 +1,23 @@
-import axios from 'axios';
-import cron from 'node-cron';
-import { MinecraftServerListPing } from 'minecraft-status';
+import cron from "node-cron";
+import { MinecraftServerListPing } from "minecraft-status";
 
-export const getPlayerCount = async (ip) => {
-
-}
+export const getPlayerCount = async () => {};
 export const getServerStatus = async (ip) => {
-    try {
-        let res = await MinecraftServerListPing.ping(process.env.MINECRAFT_PROTOCOL_VERSION, ip);
-        res.online = true;
-        return res;
-    } catch (error) {
-        return { online: false, players: { online: 0 } }
-    }
-}
+  try {
+    let res = await MinecraftServerListPing.ping(
+      process.env.MINECRAFT_PROTOCOL_VERSION,
+      ip,
+    );
+    res.online = true;
+    return res;
+  } catch (error) {
+    return { online: false, players: { online: 0 } };
+  }
+};
 
 export const monitorServerStatus = (ip, callback) => {
-    cron.schedule('* * * * *', async () => {
-        let status = await getServerStatus(ip);
-        await callback(status);
-    });
-}
+  cron.schedule("* * * * *", async () => {
+    let status = await getServerStatus(ip);
+    await callback(status);
+  });
+};
--- services/twobyte/utils.js
+++ services/twobyte/utils.js
@@ -1,17 +1,17 @@
 import { ActivityType } from "discord.js";
 
 export const updatePresence = async (status, client) => {
-    if (status.online) {
-        client.user.setStatus('online');
-    } else {
-        client.user.setStatus('dnd');
-    }
+  if (status.online) {
+    client.user.setStatus("online");
+  } else {
+    client.user.setStatus("dnd");
+  }
 
-    client.user.setActivity(
-        `${process.env.MINECRAFT_PUBLIC_IP || process.env.MINECRAFT_IP}`,
-        {
-            state: `Status: ${status.online ? "ONLINE" : "OFFLINE"} | Players: ${status.players.online}`,
-            type: ActivityType.Watching
-        }
-    );
-}
+  client.user.setActivity(
+    `${process.env.MINECRAFT_PUBLIC_IP || process.env.MINECRAFT_IP}`,
+    {
+      state: `Status: ${status.online ? "ONLINE" : "OFFLINE"} | Players: ${status.players.online}`,
+      type: ActivityType.Watching,
+    },
+  );
+};
